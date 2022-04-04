2f67403e7dd271584fbca1bfe9e8c53f0e5a8aea
Bailey Alger <balger455@gmail.com>
2022-04-04 00:04:41 -0400

    Created static, css, new pages, and wired them all together

--- resume/urls.py
+++ resume/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='resume-home'),
+    path('feedback', views.feedback, name='feedback'),
+    path('create', views.create, name='create-account'),
 ]
--- resume/views.py
+++ resume/views.py
@@ -4,3 +4,11 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'resume/home.html')
+
+
+def feedback(request):
+    return render(request, 'resume/feedback.html')
+
+
+def create(request):
+    return render(request, 'resume/createaccount.html')
