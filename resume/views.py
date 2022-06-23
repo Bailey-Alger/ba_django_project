ec9f17898a4d5af9c4f89ed8bf40fba810fc0e63
Bailey Alger <balger455@gmail.com>
2022-06-23 14:16:39 -0400

    added some feedback functionality

--- resume/views.py
+++ resume/views.py
@@ -2,15 +2,32 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import random
 
+posts = [
+    {
+        'author': 'Bailey Alger',
+        'title': 'Feedback Post 1',
+        'content': 'First post content',
+        'date_posted': 'June 23, 2022'
+    },
+    {
+        'author': 'Bob Smith',
+        'title': 'Feedback Post 2',
+        'content': 'Second post content',
+        'date_posted': 'June 23, 2022'
+    }
+]
+
 
 def home(request):
     return render(request, 'resume/home.html')
 
 
 def feedback(request):
-    return render(request, 'resume/feedback.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'resume/feedback.html', context)
 
 
 def create(request):
     return render(request, 'resume/createaccount.html')
-    
