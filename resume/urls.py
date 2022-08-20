ba1c3f549f0a6b3d2cea5db2cbafb3bfe8a5b0ae
Bailey Alger <balger455@gmail.com>
2022-08-19 20:24:08 -0400

    created user profile pictures

--- resume/urls.py
+++ resume/urls.py
@@ -7,9 +7,4 @@ urlpatterns = [
     path('', views.home, name='resume-home'),
     path('feedback/', views.feedback, name='feedback'),
     path('create/', views.create, name='create-account'),
-    path('register/', user_views.register, name='register'),
-    path('profile/', user_views.profile, name='profile'),
-    path('login/', auth_views.LoginView.as_view(
-        template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 ]
