ac66d0c37b1e2317c9e7db91fd35e5fc947e8efa
Bailey Alger <balger455@gmail.com>
2022-08-28 22:04:09 -0400

    added pagination to feedback section

--- resume/urls.py
+++ resume/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserPostListView
 
 urlpatterns = [
     path('', views.home, name='resume-home'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
     path('feedback/', PostListView.as_view(), name='feedback'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
