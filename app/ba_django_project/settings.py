63030cd35c78f59ded682c9f2e3773ffb33e1928
Bailey Alger <balger455@gmail.com>
2022-10-20 02:35:32 -0400

    created production configuration and .env.

--- app/ba_django_project/settings.py
+++ app/ba_django_project/settings.py
@@ -27,9 +27,9 @@ STATIC_DIR = os.path.join(BASE_DIR, "static")
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
