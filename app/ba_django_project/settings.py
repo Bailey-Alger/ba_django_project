8a64d4fe8ead4c8fcf98c503305f5849c7dd4d9a
Bailey Alger <balger455@gmail.com>
2023-01-07 18:14:44 -0500

    reformatted dockerfile, settings, and wsgi

--- app/ba_django_project/settings.py
+++ app/ba_django_project/settings.py
@@ -24,7 +24,7 @@ STATIC_DIR = os.path.join(BASE_DIR, "static")
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get("DEBUG", default=0))
